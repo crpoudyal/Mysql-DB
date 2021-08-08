dd5152fccebf6de19d7d89d14383ae1d9552dfd8
crpoudyal <crpoudyal@gmail.com>
2021-08-08 09:22:17 +0545

    create table with referential integrity constraints

--- createDB.sql
+++ createDB.sql
@@ -3,3 +3,5 @@ CREATE DATABASE student
 
 -- creating Tables 
 CREATE TABLE userdata(uid Integer primary key,uname varchar(20),address varchar(30),salary float)
+
+CREATE TABLE education(eid Integer primary key,degree varchar(20),uid Integer, foreign key(uid) references userdata(uid))
