2878788fbef44df77aa565b9d18fc7a9a31d5c39
crpoudyal <crpoudyal@gmail.com>
2021-08-09 17:32:36 +0545

    insert query

---
+++ insert.sql
@@ -0,0 +1,4 @@
+-- Inserting data in userdata table
+
+INSERT INTO TABLE userdata(uid,uname,address,salray) VALUES(2,'danish','surket',20000)
+INSERT INTO TABLE userdata(uid,uname,address,salray) VALUES(3,'manish','morang',25000)
