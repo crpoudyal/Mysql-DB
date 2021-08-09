e281ecea271b3fa99aeb1cad14cc10e697a5ee5f
crpoudyal <crpoudyal@gmail.com>
2021-08-09 17:46:01 +0545

    Select query

--- insert.sql
+++ insert.sql
@@ -1,5 +1,5 @@
 -- Inserting data in userdata table
 
-INSERT INTO TABLE userdata(uid,uname,address,salray) VALUES(2,'danish','surket',20000)
+INSERT INTO TABLE userdata(uid,uname,address,salary) VALUES(2,'danish','surket',20000)
 
-INSERT INTO TABLE userdata(uid,uname,address,salray) VALUES(3,'manish','morang',25000)
+INSERT INTO TABLE userdata(uid,uname,address,salary) VALUES(3,'manish','morang',25000)
--- select.sql
+++ select.sql
@@ -8,3 +8,7 @@ SELECT * FROM userdata WHERE salary > 20000 AND Address = "morang"
 
 SELECT address FROM userdata WHERE uid = 2
 
+SELECT uname,address FROM userdata WHERE salary = 20000 AND uname LIKE '%ish'
+
+SELECT salary FROM userdata WHERE uname LIKE '%is%' OR Address = 'morang'
+
