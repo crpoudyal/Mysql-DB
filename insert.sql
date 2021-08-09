c28e3232c5507b9882e99e89230ad54398b27afb
crpoudyal <crpoudyal@gmail.com>
2021-08-09 17:41:47 +0545

    Select query

--- insert.sql
+++ insert.sql
@@ -1,4 +1,5 @@
 -- Inserting data in userdata table
 
 INSERT INTO TABLE userdata(uid,uname,address,salray) VALUES(2,'danish','surket',20000)
+
 INSERT INTO TABLE userdata(uid,uname,address,salray) VALUES(3,'manish','morang',25000)
---
+++ select.sql
@@ -0,0 +1,10 @@
+-- select ___ from ___ where 
+
+SELECT * FROM userdata WHERE uname = 'danish'
+
+SELECT uname,salary FROM userdata WHERE salary < 25000
+
+SELECT * FROM userdata WHERE salary > 20000 AND Address = "morang"
+
+SELECT address FROM userdata WHERE uid = 2
+
